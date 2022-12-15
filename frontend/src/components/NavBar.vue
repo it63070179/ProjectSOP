c896d52059f1688e7422fb41b436be6161db5f73
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-12-15 21:40:03 +0700

    Create NavBar.vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-toolbar dense color="#324B4C" elevation="4" prominent>
+      <router-link to="/">
+        <v-img
+          max-height="100"
+          max-width="130"
+          src="../assets/HospitalImages.png"
+        ></v-img>
+      </router-link>
+      <v-spacer></v-spacer>
+      <v-img
+        style="margin-right: 30px; align-self: center"
+        max-height="100"
+        max-width="70"
+        src="../assets/ProfileIcon.png"
+      ></v-img>
+      <v-btn class="btn" elevation="3" rounded large color="#E63946"
+        >logout</v-btn
+      >
+    </v-toolbar>
+  </v-app>
+</template>
+<style>
+.btn {
+  color: white !important;
+  align-self: center;
+}
+.v-application--wrap {
+  min-height: 0vh !important;
+}
+</style>
+<script>
+export default {
+  name: "NavBar",
+  data: () => ({
+    dialog: false,
+    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    menu: false,
+  }),
+};
+</script>
