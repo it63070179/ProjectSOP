00a718b6f429c3d7cafb1f95a6daeaef77995955
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-12-19 17:53:12 +0700

    Delete appointment-service directory

--- appointment-service/appointment-service/src/main/java/com/example/appointmentservice/core/event/data/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.appointmentservice.core.event.data;
-
-import com.example.appointmentservice.core.AppointmentEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AppointmentRepository extends MongoRepository<AppointmentEntity, String> {
-}
