49ff23a2eb0bd36f9568ee86443765d2a1409afe
Apibun <73689795+Apibun@users.noreply.github.com>
2022-12-20 23:52:27 +0700

    Update RegisterPage.vue

--- frontend/src/views/user/RegisterPage.vue
+++ frontend/src/views/user/RegisterPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <img src="../../assets/Hospital.png" class="icon_img" />
+    <img src="../assets/Hospital.png" class="icon_img" />
     <p class="labelOnTextField" style="right: 22.5%">Name</p>
     <v-text-field label="Enter Your Name" solo v-model="v$.Name.$model" />
     <template v-if="v$.Name.$error">
@@ -13,6 +13,9 @@
       >
         This field should be at least 3 characters long
       </p>
+    </template>
+    <template v-if="this.findNameinDB !== undefined">
+      <p class="text-subtitle-2 red--text">This field is invalid</p>
     </template>
     <p class="labelOnTextField">Username</p>
     <v-text-field
@@ -30,6 +33,9 @@
       >
         This field should be at least 3 characters long
       </p>
+    </template>
+    <template v-if="this.findUsernameinDB !== undefined">
+      <p class="text-subtitle-2 red--text">This field is invalid</p>
     </template>
     <p class="labelOnTextField">Password</p>
     <v-text-field
@@ -99,20 +105,59 @@
         This field is invalid email
       </p>
     </template>
+    <template v-if="this.findEmailinDB !== undefined">
+      <p class="text-subtitle-2 red--text">This field is invalid</p>
+    </template>
     <p class="labelOnTextField">Gender</p>
     <v-select
       :items="GenderForSelect"
-      label="Gender"
+      label="Select Your Gender"
       solo
       v-model="Gender"
     ></v-select>
-    <v-btn
-      @click="addUser()"
-      depressed
-      color="#FB9C9C"
-      style="margin-bottom: 10%; width: 40%; height: 6.5%"
-      >Register</v-btn
-    >
+    <v-dialog v-model="showModal" persistent max-width="400">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          @click="addUser()"
+          depressed
+          color="#FB9C9C"
+          style="margin-bottom: 10%; width: 40%; height: 6.5%"
+          v-bind="attrs"
+          v-on="on"
+          >Register</v-btn
+        >
+      </template>
+
+      <!-- show if register success -->
+      <v-card
+        v-if="
+          v$.$invalid === false &&
+          this.Gender !== '' &&
+          this.findNameinDB === undefined &&
+          this.findUsernameinDB === undefined &&
+          this.findEmailinDB === undefined
+        "
+      >
+        <v-card-title class="text-h5"> Register Success </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="modalClick()"> OK </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card v-else>
+        <v-card-title class="text-h5"> Register not Success </v-card-title>
+        <v-card-text
+          >some field have wrong data or field require to input
+          data</v-card-text
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="showModal = false">
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <style>
@@ -199,6 +244,13 @@ export default {
       Email: "",
       GenderForSelect: ["Male", "Female", "Other"],
       Gender: "",
+      NameuserinDB: [],
+      UsernameinDB: [],
+      EmailuserinDB: [],
+      findNameinDB: undefined,
+      findUsernameinDB: undefined,
+      findEmailinDB: undefined,
+      showModal: false,
     };
   },
   validations() {
@@ -218,16 +270,64 @@ export default {
       Email: { required, minLength: minLength(3), ValidateEmail },
     };
   },
+
+  mounted() {
+    axios
+      .get("http://localhost:3001/users")
+      .then((response) => {
+        // console.log(response.data);
+        //ใส่ชื่อ user ใน DB ลง array
+        response.data.forEach((element) => {
+          this.NameuserinDB.push(element.name);
+        });
+        //ใส่username user ใน DB ลง array
+        response.data.forEach((element) => {
+          this.UsernameinDB.push(element.username);
+        });
+        //ใส่email user ใน DB ลง array
+        response.data.forEach((element) => {
+          this.EmailuserinDB.push(element.email);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
   methods: {
     addUser() {
+      this.findNameinDB = this.NameuserinDB.find(
+        (element) => element === this.Name
+      );
+      this.findUsernameinDB = this.UsernameinDB.find(
+        (element) => element === this.Username
+      );
+      this.findEmailinDB = this.EmailuserinDB.find(
+        (element) => element === this.Email
+      );
+      console.log(this.findNameinDB);
+      console.log(this.findUsernameinDB);
+      console.log(this.findEmailinDB);
+      if (this.findNameinDB !== undefined) {
+        console.log("found name invalid");
+        return;
+      }
+      if (this.findUsernameinDB !== undefined) {
+        console.log("found username invalid");
+        return;
+      }
+      if (this.findEmailinDB !== undefined) {
+        console.log("found email invalid");
+        return;
+      }
       axios
         .post("http://localhost:3000/user/register", {
           name: this.Name,
           username: this.Username,
-          passowrd: this.Password,
+          password: this.Password,
           email: this.Email,
           gender: this.Gender,
-          role : "user"
+          role: "user",
         })
         .then((response) => {
           console.log(response.data);
@@ -235,6 +335,13 @@ export default {
         .catch((err) => {
           console.log(err);
         });
+    },
+
+    modalClick() {
+      this.showModal = false;
+      this.$router.push({
+        name: "login",
+      });
     },
   },
 };
